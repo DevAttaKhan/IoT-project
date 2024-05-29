91aa0d6c051f42b9ddaefa8e73b89c3df0d0662e
devAttaKhan <attakhan220@gmail.coom>
2024-05-29 22:51:10 +0500

    fix: fixed navbar

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -5,7 +5,7 @@ type Props = { children: React.ReactElement };
 
 export const MainLayout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen  bg-gray-100">
+    <div className="min-h-screen  bg-gray-100 pb-44">
       <Header />
       <div className="p-6 max-w-[1360px] mx-auto">
         {children}
